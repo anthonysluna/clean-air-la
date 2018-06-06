96aa8bbd4a74dc9238c36080b0bead129b240199
Anthony Luna <anthony.s.luna@gmail.com>
2018-06-06 00:46:23 -0700

    initialized django backend other 'behind the scenes' data gathering and organizing. Too much info for git hub

---
+++ cleanairla/api/tests.py
@@ -0,0 +1,10 @@
+from django.test import TestCase
+from .models import pollution_profile as pp
+
+# Create your tests here.
+
+class ModelTestCase(TestCase):
+    """ This class defines the test case for the pollution_profile outputs"""
+
+    def setUp(self):
+        self.b
